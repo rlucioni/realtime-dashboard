a34ddf188786f89b4de9aec2b746bd161f68f742
Renzo Lucioni <renzo@renzolucioni.com>
2014-12-20 17:27:57 -0500

    Frontend polish

--- static/js/main.js
+++ static/js/main.js
@@ -3,8 +3,9 @@
     // connects to a server over HTTP and receives events in "text/event-stream"
     // format without closing the connection. Read more at http://goo.gl/zftAYW.
     var eventSource = new EventSource( '/stream' ),
-        eventContainer = $( '#events' ),
-        $div = $( '<div/>' );
+        $rippleContainer = $( '#ripple-container' ),
+        $ripple = $( '<div/>' ),
+        radius = 35;
 
     // The `onmessage` handler is called if no name is specified for a
     // server-sent event.
@@ -12,18 +13,18 @@
         event.preventDefault();
         console.log( event.data );
 
-        // Don't animate for keep-alive events
+        // Don't animate keep-alive events.
         if ( event.data != ':keep-alive' ) {
-            $div
+            $ripple
                 .addClass( 'circle' )
                 .css({
-                    top: eventContainer.height()/2 - 35,
-                    left: eventContainer.width()/2 - 35
+                    top: $rippleContainer.height()/2 - radius,
+                    left: $rippleContainer.width()/2 - radius
                 })
-                .appendTo( eventContainer );
+                .appendTo( $rippleContainer );
 
             window.setTimeout( function() {
-                $div.remove();
+                $ripple.remove();
             }, 2000);
         }
     };
